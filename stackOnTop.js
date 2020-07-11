b3625a5491a4a6d657e721869446b816777157be
Sunny Mistry <sunnyvmistry@gmail.com>
2020-07-11 17:12:47 -0400

    Fixed issues. Works correctly again

--- stackOnTop.js
+++ stackOnTop.js
@@ -1,28 +1,5 @@
-let innerContent = document.querySelector('.inner-content');
-let question = document.querySelector('.question');
-let questionHeader = document.querySelector('#question-header');
-let mainBar = document.querySelector('#mainbar');
-let sideBar = document.querySelector('#sidebar');
-let feedLink = document.querySelector('#feed-link');
+const mainBar = document.querySelector("#mainbar");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer.accepted-answer");
 
-innerContent.style.margin = 0;
-innerContent.style.width = "100%";
-innerContent.style.display = "flex";
-innerContent.style.paddingLeft = 0;
-innerContent.style.paddingRight = 0;
-
-question.style.overflow = "scroll";
-question.style.height = "100vh";
-question.style.clear = "none";
-question.style.paddingLeft = "15px";
-question.style.maxWidth = "50vw";
-
-mainBar.style.float = "none";
-mainBar.style.overflow = "scroll"
-mainBar.style.height = "100vh";
-
-innerContent.insertBefore(question, innerContent.firstChild);
-question.insertBefore(questionHeader, question.firstChild);
-
-sideBar.remove();
-feedLink.remove();
+mainBar.insertBefore(answer, question);
